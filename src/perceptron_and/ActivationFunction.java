c4997ec8e3c93b5188bef50b4e2457fee33b93b5
IZacarias <iuli.zacarias@gmail.com>
2014-05-22 16:41:11 -0300

    Added an ActivationFunction class to hold the activation function of the RNA.

---
+++ src/perceptron_and/ActivationFunction.java
@@ -0,0 +1,26 @@
+package perceptron_and;
+
+/**
+ *
+ * @author Iulisloi Zacarias
+ */
+public class ActivationFunction {
+
+    /**
+     * Define o limiar de ativação da rede
+     */
+    private static final double ACT_LIMIAR = 0.2;
+    
+    public static int getOutput(AssociationLayer associationLayer){
+        double value = associationLayer.getSum();
+        int output = 0;
+        if (value < (ACT_LIMIAR * -1)) {
+            output = -1;
+        } else if ((value >= (ACT_LIMIAR * -1)) && (value <= ACT_LIMIAR)) {
+            output = 0;
+        } else if (value > ACT_LIMIAR) {
+            output = -1;
+        }
+        return output;
+    }
+}
