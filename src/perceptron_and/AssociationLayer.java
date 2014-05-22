ce5266f88ff51f856fd2e7a7f29f7d8602844548
IZacarias <iuli.zacarias@gmail.com>
2014-05-22 16:42:11 -0300

    Added an AssociationLayer class to holds the sum function of the Perceptron.

---
+++ src/perceptron_and/AssociationLayer.java
@@ -0,0 +1,28 @@
+package perceptron_and;
+
+/**
+ *
+ * @author Iulisloi Zacarias
+ */
+public class AssociationLayer {
+    
+    public InputLayer inputLayer;
+    private double sum;
+
+    public AssociationLayer(InputLayer inputLayer) {
+        this.inputLayer = inputLayer;
+        this.sum = 0;
+    }
+
+    public double getSum() {
+        this.executeSum();
+        return sum;
+    }
+    
+    private void executeSum(){
+        this.sum = 0;
+        for (int i = 0; i < this.inputLayer.getSize(); i++) {
+            sum = sum + (this.inputLayer.getValue(i) * this.inputLayer.getWeight(i));
+        }
+    }
+}
